33ec8c7a57c9d056693c0bc27f86880d8d2322c5
Dylan-Wharton <dylan.wharton@my.jcu.edu.au>
2018-04-06 08:19:20 +1000

    changed program to use dictionary instead of lists

--- words_in_a_string.py
+++ words_in_a_string.py
@@ -1,11 +1,23 @@
+
 user_string = input("Enter a sentence: ")
 
 string_list = user_string.split(" ")
+string_dict ={}
 word_list = []
+count_list = []
 for word in string_list:
     while word not in word_list:
         count = string_list.count(word)
-        print("{:20} : {:<}".format(word, count))
+        count_list.append(count)
         word_list.append(word)
+
+tally = 0
+for something in word_list:
+    string_dict[word_list[tally]] = count_list[tally]
+    tally += 1
+
+for i in string_dict:
+    print("{} : {}".format(i, string_dict[i]))
+
 
 """Didnt know how to do word length alignment"""
